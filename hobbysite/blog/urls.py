cfa2897db9e3cc70380c9022717b4dfb8a332e71
hannah-admu <hannah.lim@student.ateneo.edu>
2024-05-07 03:16:41 +0800

    Finished base functionality
    everything works as per specs for blog, will make pretty soon

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import ArticleListView, ArticleDetailView
+from .views import ArticleListView, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 
 app_name = 'blog'
@@ -7,4 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name = 'article_list'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name = 'article_detail'),
+    path('article/create/', ArticleCreateView.as_view(), name = 'article_create'),
+    path('article/update/<int:pk>/', ArticleUpdateView.as_view(), name = 'article_update'),
 ]
