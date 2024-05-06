af45cbb62e3a1fa599407bd170cf63a33a5acfbe
hannah-admu <hannah.lim@student.ateneo.edu>
2024-05-07 03:16:41 +0800

    Finished base functionality
    everything works as per specs for blog, will make pretty soon

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
 
 class ArticleCategory(models.Model):
@@ -14,11 +16,16 @@ class ArticleCategory(models.Model):
     def __str__(self):
         return self.name 
     
+    def get_absolute_url(self):
+        return reverse('blog:article_list')
+    
     
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null = True)
     category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null = True)
     entry = models.TextField()
+    header_image = models.ImageField(upload_to='images/', null = True, blank = True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -30,3 +37,16 @@ class Article(models.Model):
     
     def get_absolute_url(self):
         return reverse('blog:article_detail', args=[self.pk])
+    
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f'Comment by {self.author} on {self.article}'
