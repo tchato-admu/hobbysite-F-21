d4a6abfadeef8bbfd40362eec99a22778a28cc5b
PaoloOng <paolo.ong@student.ateneo.edu>
2024-05-07 20:47:14 +0800

    added article creation and editing

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import article_list, ArticleDetailView
+from .views import article_list, ArticleDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
     path('articles', article_list, name='article_list'),
-    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail')
+    path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
+    path('article/add', ArticleCreateView.as_view(), name='article_create'),
+    path('article/<int:pk>/edit', ArticleUpdateView.as_view(), name='article_update'),
 ]
 
 app_name = 'wiki'
