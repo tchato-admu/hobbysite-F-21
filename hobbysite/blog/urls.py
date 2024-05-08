58b6a82bed980309db038f107a37da54f39e418a
hannah-admu <hannah.lim@student.ateneo.edu>
2024-05-08 11:40:11 +0800

    updated blog urls.py
    fixed the urls, everything should be according to specs now

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -7,6 +7,6 @@ app_name = 'blog'
 urlpatterns = [
     path('articles/', ArticleListView.as_view(), name = 'article_list'),
     path('article/<int:pk>/', ArticleDetailView.as_view(), name = 'article_detail'),
-    path('article/create/', ArticleCreateView.as_view(), name = 'article_create'),
-    path('article/update/<int:pk>/', ArticleUpdateView.as_view(), name = 'article_update'),
+    path('article/add/', ArticleCreateView.as_view(), name = 'article_create'),
+    path('article/<int:pk>/edit', ArticleUpdateView.as_view(), name = 'article_update'),
 ]
